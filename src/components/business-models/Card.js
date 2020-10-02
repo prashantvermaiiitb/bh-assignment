63b25ef0336c3b4ae82e5fd02c3826d5a9efbbbe
Prashant Verma <prashant.verma@timesinternet.in>
2020-10-02 12:10:08 +0530

    added components for the filtering part.

--- src/components/business-models/Card.js
+++ src/components/business-models/Card.js
@@ -5,7 +5,11 @@ import { Core } from "./Core";
  * 1. description for the card
  * 2. Terms and condition in the card
  * 3. instruction associated with card
- * 4. flags that indicate the usage for the cards.
+ * 4. related cards to show-case
+ * 5. sold out or not
+ * 6. count remaining in the inventory
+ * 7. flags that indicate the usage for the cards.
+ * 8. Popularity used for showcase in UI as well filtering them in backend
  */
 class Card extends Core {
   /**
@@ -18,6 +22,9 @@ class Card extends Core {
     this.tnc = data.tnc;
     this.instruction = data.instruction;
     this.relatedCards = new Array(data.Cards);
+    this.isSoldOut = data.isSoldOut;
+    this.count = data.count;
+    this.popularityIndex = data.popularityIndex;
     this.usageFlags = {
       isSportsCard: data.usageFlags.isSportsCard,
       isOfficeCard: data.usageFlags.isOfficeCard,
